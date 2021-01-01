1f0b3ff6627311552d6074f89cfac994800ce141
yairgueta <Yair.gueta@mail.huji.ac.il>
2021-01-01 12:58:47 +0200

    api

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Player
+{
+    public enum ResourcesType
+    {
+        Wood, 
+        Rock, 
+        Ammo,
+    }
+    
+    public class Inventory : MonoBehaviour
+    {
+        
+    }
+}

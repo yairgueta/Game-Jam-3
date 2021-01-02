da1e2c16eba7d2ebea08be9bcab1bcc7d2c20847
yairgueta <Yair.gueta@mail.huji.ac.il>
2021-01-02 20:10:31 +0200

    Selections

--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer), typeof(Collider))]
 public class Selectable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Material overMaterial = null;
+    [SerializeField] private Material clickedDownMaterial = null;
+    [SerializeField] private Material selectedMaterial = null;
+
+    private SpriteRenderer sr;
+    private Material originalMaterial;
+    private void Start()
     {
+        if (!overMaterial) overMaterial = SelectionManager.Instance.DefaultOverMaterial;
+        if (!selectedMaterial) selectedMaterial = SelectionManager.Instance.DefaultSelectedMaterial;
+        if (!clickedDownMaterial) clickedDownMaterial = SelectionManager.Instance.DefaultClickedDownMaterial;
         
+        sr = GetComponent<SpriteRenderer>();
+        originalMaterial = sr.material;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseEnter()
     {
-        
+        sr.material = overMaterial;
+    }
+
+    private void OnMouseExit()
+    {
+        sr.material = originalMaterial;
+    }
+
+    private void OnMouseDown()
+    {
+        sr.material = clickedDownMaterial;
+    }
+
+    private void OnMouseUp()
+    {
+        sr.material = selectedMaterial;
     }
 }

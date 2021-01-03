28fa26ef86a3ff5ac54386c0069214b154c72cd0
miriSilverman <mirisboom@gmail.com>
2021-01-03 03:22:09 +0200

    added player shooting

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,33 @@ public class Bullet : MonoBehaviour
     private Vector3 shootDirection;
     [SerializeField] private float bulletSpeed = 100f;
 
+    // public void Setup(Vector3 shootingDirection)
+    // {
+        // shootDirection = shootingDirection;
+        // Destroy(gameObject, 1f);
+    // }
+    
+    // void Update()
+    // {
+    //     transform.position += bulletSpeed * Time.deltaTime * shootDirection;
+    // }
+    
     public void Setup(Vector3 shootingDirection)
     {
-        shootDirection = shootingDirection;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(shootingDirection * bulletSpeed, ForceMode2D.Impulse);
         Destroy(gameObject, 1f);
     }
 
 
-    void Update()
+
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        transform.position += bulletSpeed * Time.deltaTime * shootDirection;
+        if (other.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
     }
 }

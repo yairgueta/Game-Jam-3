24b402fe05227a68e6516cc69f9625b1d0ad45a5
yairgueta <Yair.gueta@mail.huji.ac.il>
2021-01-03 21:13:40 +0200

    refactors

--- Assets/Scripts/Player/PlayerAimWeapon.cs
+++ Assets/Scripts/Player/PlayerAimWeapon.cs
@@ -1,91 +1,92 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAimWeapon : MonoBehaviour
+namespace Player
 {
+    public class PlayerAimWeapon : MonoBehaviour
+    {
 
-    private Transform aimTransform;
-    private Transform aimGunEndPoinTransform;
-    private bool ableToShoot = true;
+        private Transform aimTransform;
+        private Transform aimGunEndPoinTransform;
+        private bool ableToShoot = true;
     
-    public event EventHandler<OnShootEventArgs> OnSoot; 
-    public class OnShootEventArgs : EventArgs
-    { 
-        public Vector3 gunEndPointPos;
-        public Vector3 shootPosition;
-    }
-
-    private void Awake()
-    {
-        aimTransform = transform.Find("Aim");
-        aimGunEndPoinTransform = aimTransform.Find("GunEndPos");
-    }
-
-
-    void Update()
-    {
-        if (ableToShoot)
-        {
-            Aiming();
-            Shooting();
+        public event EventHandler<OnShootEventArgs> OnSoot; 
+        public class OnShootEventArgs : EventArgs
+        { 
+            public Vector3 gunEndPointPos;
+            public Vector3 shootPosition;
         }
 
-    }
-
-    private Vector3 GetMousePos()
-    {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0f;
-        return mousePosition;
-    }
-    
-    
-    // aiming the gun at the direction og the mouse
-    private void Aiming()
-    {
-        Vector3 mousePosition = GetMousePos();
-        Vector3 aimDirection = (mousePosition - transform.position).normalized;
-        // angle in radians converted to angle in deg
-        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-        aimTransform.eulerAngles = new Vector3(0,0, angle);
-        
-        // rotate the gun to the right direction
-        Vector3 aimLocalScale = Vector3.one;
-        if (angle > 90 || angle < -90)
+        private void Awake()
         {
-            aimLocalScale.y = -1f;
-        }
-        else
-        {
-            aimLocalScale.y = +1f;
+            aimTransform = transform.Find("Aim");
+            aimGunEndPoinTransform = aimTransform.Find("GunEndPos");
         }
 
-        aimTransform.localScale = aimLocalScale;
 
+        void Update()
+        {
+            if (ableToShoot)
+            {
+                Aiming();
+                Shooting();
+            }
 
-    }
+        }
 
-    private void Shooting()
-    {
-        if (Input.GetMouseButtonDown(0))
+        private Vector3 GetMousePos()
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;
+            return mousePosition;
+        }
+    
+    
+        // aiming the gun at the direction og the mouse
+        private void Aiming()
         {
             Vector3 mousePosition = GetMousePos();
-
-            OnSoot?.Invoke(this, new OnShootEventArgs
+            Vector3 aimDirection = (mousePosition - transform.position).normalized;
+            // angle in radians converted to angle in deg
+            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+            aimTransform.eulerAngles = new Vector3(0,0, angle);
+        
+            // rotate the gun to the right direction
+            Vector3 aimLocalScale = Vector3.one;
+            if (angle > 90 || angle < -90)
             {
-                gunEndPointPos = aimGunEndPoinTransform.position,
-                shootPosition = mousePosition,
-                    
-            });
+                aimLocalScale.y = -1f;
+            }
+            else
+            {
+                aimLocalScale.y = +1f;
+            }
+
+            aimTransform.localScale = aimLocalScale;
+
+
         }
+
+        private void Shooting()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mousePosition = GetMousePos();
+
+                OnSoot?.Invoke(this, new OnShootEventArgs
+                {
+                    gunEndPointPos = aimGunEndPoinTransform.position,
+                    shootPosition = mousePosition,
+                    
+                });
+            }
+        }
+    
+    
+    
+    
+    
+    
+    
     }
-    
-    
-    
-    
-    
-    
-    
 }

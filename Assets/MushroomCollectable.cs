11e137f9331266652bfdf0a1055fc887ec20828e
miriSilverman <mirisboom@gmail.com>
2021-01-13 17:24:52 +0200

    mushroom

--- Assets/MushroomCollectable.cs
+++ Assets/MushroomCollectable.cs
@@ -2,21 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Collectables;
+using Events;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Collectables/Mushroom_@@")]
 
 public class MushroomCollectable : CollectableObject
 {
-    [SerializeField] private Spawner spawner;
+    public GameEvent onMushroomCollect;
 
-    private void OnEnable()
-    {
-        spawner = GameObject.Find("SheepSpawner").GetComponent<Spawner>();
-    }
 
     public override void OnCollected()
     {
-        spawner.SpawnGameObject();
+        onMushroomCollect.Raise();
     }
 }

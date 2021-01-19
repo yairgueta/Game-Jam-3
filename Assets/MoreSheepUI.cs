36ddba8a0674791cadd9621b41d0709b06d59365
yairgueta <Yair.gueta@mail.huji.ac.il>
2021-01-19 11:01:52 +0200

    UI

--- Assets/MoreSheepUI.cs
+++ Assets/MoreSheepUI.cs
@@ -8,14 +8,18 @@ using UnityEngine.UI;
 
 public class MoreSheepUI : MonoBehaviour
 {
-    [SerializeField] private float enterDuration, exitDuration, onScreenDuration, f;
+    [SerializeField] private float enterDuration, exitDuration, onScreenDuration;
     [SerializeField] private GameEvent mushroomCollectedEvent;
+    [SerializeField] private Ease ease;
     private Tween tween;
     private Image image;
+    private RectTransform rt;
 
     private void Start()
     {
         image = GetComponent<Image>();
+        rt = GetComponent<RectTransform>();
+        
         var listener = gameObject.AddComponent<GameEventListener>();
         listener.InitEvent(mushroomCollectedEvent);
         listener.response.AddListener(o => OnSheepCollectedAnimation());
@@ -26,8 +30,9 @@ public class MoreSheepUI : MonoBehaviour
         tween?.Kill(true);
         image.color = Color.white;
         tween = DOTween.Sequence()
-            .Append(gameObject.transform.DOMoveY(f, enterDuration).From().SetEase(Ease.OutBounce))
+            .Append(rt.DOMoveY(0, enterDuration).From().SetEase(ease))
             .Append(image.DOFade(0f, exitDuration).SetDelay(onScreenDuration));
+            // rt.position = new Vector3(rt.position.x, 0);
     }
     
     

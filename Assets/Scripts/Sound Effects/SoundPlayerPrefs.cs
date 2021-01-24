62055980503fa510998ccd7ffc3147efbf23898e
Elran Asher <elran2030@hotmail.com>
2021-01-24 19:20:31 +0200

    added music to main menu

--- Assets/Scripts/Sound Effects/SoundPlayerPrefs.cs
+++ Assets/Scripts/Sound Effects/SoundPlayerPrefs.cs
@@ -8,12 +8,15 @@ public class SoundPlayerPrefs : MonoBehaviour
     [SerializeField] private SoundSettings soundSettings;
     [SerializeField] private Slider bgMusicSlider;
     [SerializeField] private Slider sfxMusicSlider;
+    private AudioSource menuAudioSource;
     
     // Start is called before the first frame update
     void Start()
     {
+        menuAudioSource = gameObject.GetComponent<AudioSource>();
         bgMusicSlider.value = soundSettings.bgVolume;
         sfxMusicSlider.value = soundSettings.sfxVolume;
+        menuAudioSource.volume = soundSettings.bgVolume;
     }
 
     public void ChangeSFXVolume(float value)
@@ -23,6 +26,7 @@ public class SoundPlayerPrefs : MonoBehaviour
 
     public void ChangeBGVolume(float value)
     {
+        menuAudioSource.volume = value;
         soundSettings.bgVolume = value;
     }
 }

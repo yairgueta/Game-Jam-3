84a967e38170cce5b15ad07792a6ab5d7198fbcb
miriSilverman <mirisboom@gmail.com>
2021-01-31 15:44:41 +0200

    deddd

--- Assets/Scripts/MinimapIcons.cs
+++ Assets/Scripts/MinimapIcons.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cycles;
+using DG.Tweening;
 using UnityEngine;
 using Object = System.Object;
 
 public class MinimapIcons : MonoBehaviour
 {
     private Animator anim;
-    // Start is called before the first frame update
+    private Tween tween;
+    private SpriteRenderer sheepRenderer;
+    
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -21,6 +24,10 @@ public class MinimapIcons : MonoBehaviour
     public void StartAttack(object o)
     {
         anim.SetBool("isAttacking", true);
+        // tween = DOTween.Sequence()
+            // .Append(DOTween.To(() => sheepRenderer.color.a, f => sheepRenderer.color = f, 0f, 1f))
+            // .Append(DOTween.To(() => light2D.intensity, f => light2D.intensity = f, 1.24F, 0.2f).SetDelay(0.1f));
+
     }
     
     public void DoneAttack(object o)

d8f4553c588fae93d471cf808b16227d1ae7fc38
yairgueta <Yair.gueta@mail.huji.ac.il>
2021-02-02 02:58:12 +0200

    UI

--- Assets/Scripts/Cycles/CyclesManager.cs
+++ Assets/Scripts/Cycles/CyclesManager.cs
@@ -10,6 +10,7 @@ namespace Cycles
     public class CyclesManager : MonoBehaviour
     {
         public static CyclesManager Instance { get; private set; }
+        public int DaysCount => cyclesCount / 3;
 
         public IEnumerable<CycleObject> CyclesSettings => cyclesSettings;
         public CycleObject DaySettings => cyclesSettings[0];
@@ -22,6 +23,7 @@ namespace Cycles
         private CycleObject currentCycle;
         private Queue<CycleObject> cyclesQueue;
         private float timer;
+        private int cyclesCount;
 
         private void OnEnable()
         {
@@ -53,20 +55,21 @@ namespace Cycles
 
         private void ProgressCycle()
         {
+            cyclesCount++;
             currentCycle = cyclesQueue.Dequeue();
             cyclesQueue.Enqueue(currentCycle);
             timer = currentCycle.Duration;
             currentCycle.OnCycleStart.Raise();
         }
 
-        private Rect r = new Rect(Screen.width * .8f, 0, Screen.width * .2f, Screen.height * .1f);
-        private StringBuilder sb = new StringBuilder(20,20);
-        private void OnGUI()
-        {
-            sb.Clear();
-            sb.Append(currentCycle.name).Append(": ").Append(Math.Round(timer, 2));
-            GUI.Label(r, sb.ToString());
-        }
+        // private Rect r = new Rect(Screen.width * .8f, 0, Screen.width * .2f, Screen.height * .1f);
+        // private StringBuilder sb = new StringBuilder(20,20);
+        // private void OnGUI()
+        // {
+        //     sb.Clear();
+        //     sb.Append(currentCycle.name).Append(": ").Append(Math.Round(timer, 2));
+        //     GUI.Label(r, sb.ToString());
+        // }
         
 
 
